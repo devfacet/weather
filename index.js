6176ab06202bbeaad3dffdb9960eefee708a32be
Fatih Cetinkaya <cmuratfatih@gmail.com>
2017-03-14 22:03:36 -0400

    Improve address and error handling

--- index.js
+++ index.js
@@ -17,21 +17,20 @@ module.exports = (function() {
       defLang       = 'en-US',
       defDegreeType = 'F',
       defTimeout    = 10000,
-      findUrl       = 'http://weather.service.msn.com/find.aspx',
-      find;         // finds and gets weather information - function
+      findUrl       = 'http://weather.service.msn.com/find.aspx';
 
-  find = function find(options, callback) {
+  var find = function find(options, callback) {
 
     if(typeof callback !== 'function')
       callback = function callback(err, result) { return err || result; };
 
     if(!options || typeof options !== 'object')
-      return callback('Invalid options!');
+      return callback('invalid options');
 
     if(!options.search)
-      return callback('Missing search input!');
+      return callback('missing search input');
 
-    var result,
+    var result     = [],
         lang       = options.lang || defLang,
         degreeType = options.degreeType || defDegreeType,
         timeout    = options.timeout || defTimeout,
@@ -40,22 +39,33 @@ module.exports = (function() {
 
     request.get({url: reqUrl, timeout: timeout}, function(err, res, body) {
 
-      if(err) return callback(err);
-      if(res.statusCode !== 200) return callback('Request failed (' + res.statusCode + ')');
+      if(err)                    return callback(err);
+      if(res.statusCode !== 200) return callback(new Error('request failed (' + res.statusCode + ')'));
+      if(!body)                  return callback(new Error('failed to get body content'));
 
+      // Check body content
+      if(body.indexOf('<') !== 0) {
+        if(body.search(/not found/i) !== -1) {
+          return callback(null, result);
+        }
+        return callback(new Error('invalid body content'));
+      }
+
+      // Parse body
       xmlParser.parseString(body, function(err, resultJSON) {
         if(err) return callback(err);
 
         if(!resultJSON || !resultJSON.weatherdata || !resultJSON.weatherdata.weather)
-          return callback('Unexpected error! Invalid content.');
+          return callback(new Error('failed to parse weather data'));
 
         if(resultJSON.weatherdata.weather['A$'] && resultJSON.weatherdata.weather['A$'].errormessage)
           return callback(resultJSON.weatherdata.weather['A$'].errormessage);
 
         if(!(resultJSON.weatherdata.weather instanceof Array)) {
-          return callback('Unexpected error! Missing weather info.');
+          return callback(new Error('missing weather info'));
         }
 
+        // Iterate over weather data
         var weatherLen = resultJSON.weatherdata.weather.length,
             weatherItem;
         for(var i = 0; i < weatherLen; i++) {
@@ -63,6 +73,7 @@ module.exports = (function() {
           if(typeof resultJSON.weatherdata.weather[i]['A$'] !== 'object')
             continue;
 
+          // Init weather item
           weatherItem = {
             location: {
               name: resultJSON.weatherdata.weather[i]['A$']['weatherlocationname'],
@@ -98,16 +109,15 @@ module.exports = (function() {
             }
           }
 
-          if(!result) result = [];
+          // Push weather item into result
           result.push(weatherItem);
         }
 
-        return callback(undefined, result);
+        return callback(null, result);
       });
     });
   };
 
-  // Return
   return {
     find: find
   };
